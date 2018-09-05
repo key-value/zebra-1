6f2ce1d2ab2700f06a8f1fa02fe8b4a0ab8c0f73
wuyou <yinyazuolun@sina.com>
2018-09-06 00:06:28 +0800

    初始化项目 回收之前的页面，继续工作，将页面分为左右结构

---
+++ src/components/Default.vue
@@ -0,0 +1,67 @@
+<template >
+    <section>
+        <aside class="sidebar Red">3
+        </aside>
+        <section>
+            <nav class="navbar orangered">2</nav>
+            <section class="content yellow">5
+                <article class="blue">4</article>
+            </section>
+            <footer class="green">6</footer>
+        </section>
+    </section>
+</template>
+
+<script>
+import SystemHeader from "./System/SystemHeader";
+
+export default {
+  name: "Main",
+  components: {
+    SystemHeader
+  },
+  data() {
+    return {
+      counter: 0
+    };
+  }
+};
+</script>
+
+<style scopedSlots>
+.navbar {
+  position: relative;
+  height: 3rem;
+  width: calc(100% - 250px);
+  right: 0%;
+  padding: 30px;
+}
+.sidebar {
+  position: fixed;
+  top: 0px;
+  height: 100vh;
+  width: 250px;
+}
+.content {
+  display: inline-block;
+  position: relative;
+}
+.Red {
+  background: red;
+}
+.black {
+  background: black;
+}
+.blue {
+  background: blue;
+}
+.yellow {
+  background: yellow;
+}
+.green {
+  background: green;
+}
+.orangered {
+  background: orangered;
+}
+</style>
---
+++ src/components/System/SystemHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav>
+        <ul>
+            <li v-for="navlogo in navLogoList" :key="navlogo">
+                <a> {{navlogo}}</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: "SystemHeader",
+  data() {
+    return {
+      navLogoList: [1, 2, 3, 4]
+    };
+  }
+};
+</script>
+
+<style scoped>
+li {
+  display: inline-block;
+  background-color: black;
+}
+</style>
